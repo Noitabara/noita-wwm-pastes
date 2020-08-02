108090b258c6a3c2ef3b6887a188339cd8ebb21f
Gray <xxbloodyhell478@gmail.com>
2020-08-01 22:30:27 -0400

    Instance set up

--- src/Mod.ts
+++ src/Mod.ts
@@ -1,5 +1,16 @@
 import Mod from "mod/Mod";
+import Log from "utilities/Log";
+
+let log: Log
 
 export default class Pastes extends Mod {
+    public onInitialize() {
+        log = this.getLog()
+        log.info('Hello, sweet world.')
+    }
+
+    @Mod.instance<Pastes>("Buff Pastes")
+    public static readonly INSTANCE: Pastes
+
 
 }
