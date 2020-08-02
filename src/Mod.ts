a99b540600654b8b693e3c4fce1ee41beea206a0
Gray <xxbloodyhell478@gmail.com>
2020-08-01 23:09:32 -0400

    Implemented the stam buff class extention

--- src/Mod.ts
+++ src/Mod.ts
@@ -1,5 +1,9 @@
 import Mod from "mod/Mod";
 import Log from "utilities/Log";
+import StatusEffect, { IStatusEffectIconDescription } from "entity/status/StatusEffect";
+import { Stat } from "entity/IStats";
+import { StatChangeReason, StatusType } from "entity/IEntity";
+import { StatNotificationType } from "renderer/INotifier";
 
 let log: Log
 
@@ -14,16 +18,50 @@ interface IUsersBuffData {
     [key: string]: IStamPasteData
 }
 
+class StaminaBuff extends StatusEffect {
+    @Override
+    getIcon(): IStatusEffectIconDescription {
+        // Check in steam deploy for where this is actually resolving. Should know pretty fast that it's failing lol.
+        return {
+            path: '../../mods/buff_pastes/static/image/item/stambuff_8.png'
+        }
+    }
+    @Override
+    onTick(): void {
+        // Refrences the buffData variable in the Pastes mod class.
+        let locPlayersData = Pastes.INST.buffData[this.entity.asPlayer!.identifier]
+
+        // "Calculate"(lol) the effect multiplier based on the passed in variables from the item used.
+        let effectMultiplier = Math.floor((locPlayersData.PasteBuffMinDura / locPlayersData.PasteBuffMaxDura * locPlayersData.PasteBuffQuality) + 1)
+        // Increase the stat Stamina by the effect multipler
+        this.entity.asPlayer?.stat.increase(Stat.Stamina, effectMultiplier, StatChangeReason.Normal)
+        // call the notifyStat function to indicate that the stat has increased by the effect multiplier
+        this.entity.asPlayer?.notifyStat(StatNotificationType.Metabolism, effectMultiplier)
+
+        // Iterate the buff tick by 1 because infinite buffs would be a bit OP.
+        locPlayersData.PasteBuffTick++
+    }
+    @Override
+    shouldPass(): boolean {
+        // If the buff has ticked 5 times, set the buffData ticker to 0 and pass the effect by returning !false(lol)
+        if (Pastes.INST.buffData[this.entity.asPlayer!.identifier].PasteBuffTick >= 5) {
+            Pastes.INST.buffData[this.entity.asPlayer!.identifier].PasteBuffTick = 0
+            return true
+        }
+        return false
+    }
+}
+
 export default class Pastes extends Mod {
     public buffData: IUsersBuffData = {}
-    
+
     public onInitialize() {
         log = this.getLog()
         log.info('Hello, sweet world.')
     }
 
     @Mod.instance<Pastes>("Buff Pastes")
-    public static readonly INSTANCE: Pastes
+    public static readonly INST: Pastes
 
     
 }
