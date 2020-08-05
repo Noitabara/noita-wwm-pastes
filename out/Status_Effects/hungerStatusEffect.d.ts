fe24f9de6e52f85ada381e24e9e135e170e2917a
Gray <xxbloodyhell478@gmail.com>
2020-08-05 01:19:14 -0400

    out/Status_Effects/hungerStatusEffect.: Deleted, unused && un-needed

--- out/Status_Effects/hungerStatusEffect.d.ts
+++
@@ -1,4 +0,0 @@
-import StatusEffect, { IStatusEffectIconDescription } from "entity/status/StatusEffect";
-export default class HungerBuff extends StatusEffect {
-    getIcon(): IStatusEffectIconDescription;
-}
