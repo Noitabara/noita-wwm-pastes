6995fb91e68b1c48241de2991aa4da0cb5740d88
Gray <xxbloodyhell478@gmail.com>
2020-08-18 21:23:20 -0400

    testing weight fix pt.1

--- src/statuses.ts
+++ src/statuses.ts
@@ -3,7 +3,7 @@ import { IWeightPasteData, IStamPasteData } from "./storageEnums"
 import StatusEffect, { StatusEffectBadness, IStatusEffectIconDescription } from "entity/status/StatusEffect";
 import { Stat } from "entity/IStats";
 import { StatChangeReason } from "entity/IEntity";
-import { StatNotificationType } from "renderer/INotifier";
+// import { StatNotificationType } from "renderer/INotifier";
 import Translation from "language/Translation";
 
 // TODO: Fix "dirty" var assignments with class properties later- (onTick, shouldPass, getDescription).
@@ -32,14 +32,14 @@ export class StaminaBuff extends StatusEffect {
         // Refrences the buff_stam_data variable in the Pastes mod class.
         const locPlayersData = Pastes.INST.buff_stam_data[this.entity.asPlayer!.identifier]
 
-        // "Calculate"(lol) the effect multiplier based on the passed in variables from the item used.
-        const effectTickAmount = Math.floor((locPlayersData.PasteBuffMinDura / locPlayersData.PasteBuffMaxDura * locPlayersData.PasteBuffQuality) + 1)
-        // Increase the stat Stamina by the effect multipler
-        this.entity.asPlayer?.stat.increase(Stat.Stamina, effectTickAmount, StatChangeReason.Normal)
-        // call the notifyStat function to indicate that the stat has increased by the effect multiplier
-        this.entity.asPlayer?.notifyStat(StatNotificationType.Stamina, effectTickAmount)
+        // // "Calculate"(lol) the effect multiplier based on the passed in variables from the item used.
+        // const effectTickAmount = Math.floor((locPlayersData.PasteBuffMinDura / locPlayersData.PasteBuffMaxDura * locPlayersData.PasteBuffQuality) + 1)
+        // // Increase the stat Stamina by the effect multipler
+        // this.entity.asPlayer?.stat.increase(Stat.Stamina, effectTickAmount, StatChangeReason.Normal)
+        // // call the notifyStat function to indicate that the stat has increased by the effect multiplier
+        // this.entity.asPlayer?.notifyStat(StatNotificationType.Stamina, effectTickAmount)
 
-        this.entity.asPlayer?.stat.setBonus(Stat.Weight, 100, StatChangeReason.BonusChanged)
+        // this.entity.asPlayer?.stat.setBonus(Stat.Weight, 100, StatChangeReason.BonusChanged)
 
         // Iterate the buff tick by 1 because infinite buffs would be a bit OP.
         locPlayersData.PasteBuffTick++
